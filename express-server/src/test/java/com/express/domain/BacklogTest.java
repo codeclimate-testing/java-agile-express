3186df1dbf5282eca749bb1d94464849ee2cb533
Adam Boas <adam.boas@gmail.com>
2010-02-04 10:22:38 +1100

    Fixed schedule for writing effort records to happen at night not day

--- express-server/src/test/java/com/express/domain/BacklogTest.java
+++ express-server/src/test/java/com/express/domain/BacklogTest.java
@@ -9,19 +9,30 @@ import org.junit.Before;
 import org.junit.Test;
 import org.unitils.UnitilsJUnit4;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class BacklogTest extends UnitilsJUnit4 {
    private static final Log LOG = LogFactory.getLog(BacklogTest.class);
    static final String REF = "ref-1";
+   static final Theme THEME = new Theme();
+   static final String THEME_TITLE = "test";
    static final String TITLE = "test title";
    static final String SUMMARY = "test summary";
    static final String STATUS = Status.DONE.getTitle();
    static final String ASSIGENED_TO = "test user";
+   static final Integer EFFORT = 5;
+   static final Integer BUSINESS_VALUE = 4;
 
    BacklogItem item;
 
    @Before
    public void setUp() {
       item = new BacklogItem();
+      THEME.setTitle(THEME_TITLE);
+      Set<Theme> themes = new HashSet<Theme>();
+      themes.add(THEME);
+      item.setThemes(themes);
    }
 
    @Test
@@ -32,7 +43,9 @@ public class BacklogTest extends UnitilsJUnit4 {
    }
 
    @Test
-   public void CSVShouldMatcheKeyFields() {
+   public void CSVShouldMatchKeyFields() {
+      item.setEffort(EFFORT);
+      item.setBusinessValue(BUSINESS_VALUE);
       item.setReference(REF);
       item.setTitle(TITLE);
       item.setSummary(SUMMARY);
@@ -41,7 +54,8 @@ public class BacklogTest extends UnitilsJUnit4 {
       user.setFirstName("test");
       user.setLastName("user");
       item.setAssignedTo(user);
-      String expected = REF + "," + TITLE + "," + SUMMARY + "," + STATUS + "," + ASSIGENED_TO;
+      String expected = REF + "," + THEME_TITLE + " ," + TITLE + "," + SUMMARY + "," + STATUS + "," + ASSIGENED_TO+ ","
+                        + EFFORT + "," + BUSINESS_VALUE;
       assertEquals(expected, item.toCSV());
    }
 
