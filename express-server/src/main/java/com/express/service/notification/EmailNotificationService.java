3812c08dfffb4a39b0e2e8fa616fa32f80fd68a6
Adam Boas <adam.boas@gmail.com>
2011-04-09 17:45:42 +1000

    Allowed Mail notifications to be switched off

--- express-server/src/main/java/com/express/service/notification/EmailNotificationService.java
+++ express-server/src/main/java/com/express/service/notification/EmailNotificationService.java
@@ -10,41 +10,53 @@ import com.express.domain.AccessRequest;
 
 @Service("notificationService")
 public class EmailNotificationService implements NotificationService {
-   
+
    private final NotificationFactory notificationFactory;
    private final MailSender mailSender;
    private final PropertiesConfiguration config;
-   
+   private final boolean emailEnabled;
+
    @Autowired
-   public EmailNotificationService(@Qualifier("mailSender")MailSender mailSender,
-                                  @Qualifier("config")PropertiesConfiguration config,
-                                  @Qualifier("notificationFactory")NotificationFactory notificationFactory) {
+   public EmailNotificationService(@Qualifier("mailSender") MailSender mailSender,
+                                   @Qualifier("config") PropertiesConfiguration config,
+                                   @Qualifier("notificationFactory") NotificationFactory notificationFactory) {
       this.mailSender = mailSender;
       this.notificationFactory = notificationFactory;
       this.config = config;
+      this.emailEnabled = this.config.getBoolean("email.enabled");
    }
 
    public void sendConfirmationNotification(User user) {
-      String url = config.getString("applicationUrl") + "/express.html#registerId=" + user.getId();
-      mailSender.sendMail(notificationFactory.createConfirmationNotification(user, url));
+      if (emailEnabled) {
+         String url = config.getString("applicationUrl") + "/express.html#registerId=" + user.getId();
+         mailSender.sendMail(notificationFactory.createConfirmationNotification(user, url));
+      }
    }
 
    public void sendPasswordReminderNotification(User user) {
-      mailSender.sendMail(notificationFactory.createPasswordReminderNotification(user));
+      if (emailEnabled) {
+         mailSender.sendMail(notificationFactory.createPasswordReminderNotification(user));
+      }
    }
 
    public void sendProjectAccessRequestNotification(AccessRequest request, User manager) {
-      mailSender.sendMail(notificationFactory.createProjectAccessRequestNotification(
-                          request.getRequestor(),request.getProject(), manager));
+      if (emailEnabled) {
+         mailSender.sendMail(notificationFactory.createProjectAccessRequestNotification(
+               request.getRequestor(), request.getProject(), manager));
+      }
    }
 
    public void sendProjectAccessAccept(AccessRequest request) {
-      mailSender.sendMail(notificationFactory.createProjectAccessAcceptNotification(
-                                                      request.getRequestor(),request.getProject()));
+      if (emailEnabled) {
+         mailSender.sendMail(notificationFactory.createProjectAccessAcceptNotification(
+               request.getRequestor(), request.getProject()));
+      }
    }
 
    public void sendProjectAccessReject(AccessRequest request) {
-      mailSender.sendMail(notificationFactory.createProjectAccessRejectNotification(
-                                                      request.getRequestor(),request.getProject()));
+      if (emailEnabled) {
+         mailSender.sendMail(notificationFactory.createProjectAccessRejectNotification(
+               request.getRequestor(), request.getProject()));
+      }
    }
 }
